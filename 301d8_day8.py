0c4d0222e2458c52180f44b8a1e34d3a67f5f97b
divermedic <divermedic22.lab@gmail.com>
2023-06-09 15:47:35 +0000

    day 8 code

---
+++ 301d8_day8.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
