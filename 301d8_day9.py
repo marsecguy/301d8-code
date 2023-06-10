7d4117f86a71db4018904695e82041c0a8b06713
divermedic <divermedic22.lab@gmail.com>
2023-06-10 01:17:51 +0000

    day 9 code challenge

---
+++ 301d8_day9.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+
+a = 5
+b = 10
+
+# Equals: a == b
+if a == b:
+    print("same")
+else:
+    print('not the same')  
+
+# Not Equals: a != b
+if a != b:
+    print("true")
+else:
+    print("false")
+
+# Less than: a < b
+if a < b:
+    print("sure")
+else: 
+    print("nope")
+    
+# Less than or equal to: a <= b
+if a <= b:
+    print("yes")
+else:
+    print("no")
+
+# Greater than: a > b
+if a > b:
+    print('da')
+else:
+    print("nyet")
+
+# Greater than or equal to: a >= b
+if a >= b:
+    print("jah")
+else:
+    print("nein") 
