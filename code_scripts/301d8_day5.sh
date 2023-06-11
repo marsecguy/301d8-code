f50f8948b3fd7b437dd8fa73b62086c2e62c3eb7
divermedic <divermedic22.lab@gmail.com>
2023-06-11 23:24:50 +0000

    day 5 code

--- code_scripts/301d8_day5.sh
+++ code_scripts/301d8_day5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Script Name:                  Script Name
-# Author:                       Your Name
-# Date of latest revision:      00/00/0000
-# Purpose:                      Purpose
+# Script Name:                  Clearing logs
+# Author:                       Chris Bennett 
+# Date of latest revision:      06/11/23
+# Purpose:                      Compress and clear logs
 
 # Declaration of variables
 
@@ -11,6 +11,30 @@
 
 
 # Main
+# Print to the screen the file size
+echo "File Size and Name:"
+stat -c '%s' /var/log/syslog /var/log/wtmp
+
+echo 
+# Compress Files into Backup Directory and TimeStamp
+echo 
+zip -r Backup-Directory$(date +"%Y%m%d%H%M%S").zip /var/log/syslog /var/log/wtmp
+echo "Backup has been Created"
+echo 
+
+# Print compressed file sizes
+echo "Compressed File Size:"
+gzip -l Backup-Directory$(date +"%Y%m%d%H%M%S").zip
+echo
+
+# Clear File Content
+sudo truncate -s 0 /var/log/syslog /var/log/wtmp
+
+# Compare sizes
+echo "Comparison:"
+du -h /var/log/syslog /var/log/wtmp
+du -h Backup-Directory$(date +"%Y%m%d%H%M%S").zip
+echo 
 
 
 
