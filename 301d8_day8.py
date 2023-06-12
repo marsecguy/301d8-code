db12dc2b685479c1d6e75540d709fbc21af0b331
divermedic <divermedic22.lab@gmail.com>
2023-06-12 00:07:36 +0000

    day 8 code

--- 301d8_day8.py
+++ 301d8_day8.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
+# Script Name:                  Lists in Python
+# Author:                       Chris Bennett
+# Date of latest revision:      06/11/23
+# Purpose:                      Manipultaing lists in Python
+
+
 # Assign to a variable a list of ten string elements.
+Norse_Dieties = ["Odin", "Thor", "Frejya", "Tyr", "Ullr", "Njord", "Loki", "Beyla", "Eir", "Baldr"]
+
 # Print the fourth element of the list.
+print("Forth Aesir:", Norse_Dieties[3])
+
 # Print the sixth through tenth element of the list.
+print("Sixth to Tenth Aesir:", Norse_Dieties[5:])
+
 # Change the value of the seventh element to “onion”.
+Norse_Dieties[6] = "Onion"
+print("Updated list:", Norse_Dieties)
