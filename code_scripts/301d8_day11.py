d1e4fdec0b25e4463af1e87dcf42cb3d7f5f3099
divermedic <divermedic22.lab@gmail.com>
2023-06-14 14:12:00 +0000

    day 11 code

---
+++ code_scripts/301d8_day11.py
@@ -0,0 +1,26 @@
+#!/bin/bash/env python3
+
+# Script Name:                  PSUTIL
+# Author:                       Chris Bennett
+# Date of latest revision:      06/14/23
+# Purpose:                      Using psutil command
+
+# Create a Python script that fetches this information using Psutil:
+
+# Time spent by normal processes executing in user mode
+
+# Time spent by processes executing in kernel mode
+
+# Time when system was idle
+
+# Time spent by priority processes executing in user mode
+
+# Time spent waiting for I/O to complete.
+
+# Time spent for servicing hardware interrupts
+
+# Time spent for servicing software interrupts
+
+# Time spent by other operating systems running in a virtualized environment
+
+# Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
